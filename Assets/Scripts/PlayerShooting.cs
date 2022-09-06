7793fca2012dc0c9d23a15014edcc823260a1a61
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2022-09-06 22:56:33 +0200

    Improved aiming

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -7,35 +7,42 @@ public class PlayerShooting : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _shotSparkParticle;
     [SerializeField] private float _range = 100f;
+    [SerializeField] private float _shotVelocity = 10f;
     private bool _fired = false;
     Vector3 _firedFromPosition;
-    Vector3 _hitPosition;
+    Vector3 _targetPosition;
+    Vector3 _shotDirection;
+    float _maxDistance;  // will be in weapon class when its created
 
     private void Update() 
     {
-        Vector3 screenPosition = Input.mousePosition;
-        screenPosition.z = Camera.main.nearClipPlane + 1;
-
-        Ray cameraToMouseRay = Camera.main.ScreenPointToRay(screenPosition);
-        
-        if (Input.GetKeyDown(KeyCode.Mouse0) && Physics.Raycast(cameraToMouseRay, out RaycastHit hitData, _range)) 
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            // _shotSparkParticle.transform.position = hitData.point;
+            Vector3 screenPosition = Input.mousePosition;
+            screenPosition.z = Camera.main.nearClipPlane + 1;
+            Ray cameraToMouseRay = Camera.main.ScreenPointToRay(screenPosition);
+
+            Vector3 cameraToPlayerVector = transform.position - Camera.main.transform.position;
+
+            _targetPosition = cameraToMouseRay.direction * _range + Camera.main.transform.position;
+            _shotDirection = Vector3.Normalize(cameraToMouseRay.direction * _range - cameraToPlayerVector);
+            _firedFromPosition = transform.position;
+            _maxDistance = Vector3.Magnitude(_targetPosition - transform.position);
+
             _shotSparkParticle.Play();
             _shotSparkParticle.transform.position = transform.position;
             _firedFromPosition = transform.position;
-            _hitPosition = hitData.point;
             _fired = true;
         }
-
 
         if (_fired) { MoveSpark(); }
     }
 
     private void MoveSpark()
     {
-        _shotSparkParticle.transform.position += Vector3.Normalize(_hitPosition - _firedFromPosition) * 10 * Time.deltaTime;
-        if (Vector3.Magnitude(_shotSparkParticle.transform.position - _firedFromPosition) >= _range) 
+        _shotSparkParticle.transform.position += _shotDirection * _shotVelocity * Time.deltaTime;
+        float distanceFromTarget = Vector3.Magnitude(_shotSparkParticle.transform.position - _firedFromPosition);
+        if (distanceFromTarget >= _maxDistance) 
         {
             _shotSparkParticle.Stop();
             _fired = false;
