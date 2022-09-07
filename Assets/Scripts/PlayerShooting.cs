fcb9a135ce73514666729d8886e60f048e4a2f8f
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2022-09-07 15:13:53 +0200

    Improved shooting
    Added a placeholder weapon model. Added bullet particles for shooting
    which shoot from weapon muzzle. Added raycasting for when mouse position
    hits a game object and when it doesn't find a game object, it calculates
    the target position and direction using the given max distance.

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -5,47 +5,17 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem _shotSparkParticle;
-    [SerializeField] private float _range = 100f;
-    [SerializeField] private float _shotVelocity = 10f;
-    private bool _fired = false;
-    Vector3 _firedFromPosition;
-    Vector3 _targetPosition;
-    Vector3 _shotDirection;
-    float _maxDistance;  // will be in weapon class when its created
+    [SerializeField] Weapon _currentWeapon;
+    private AimDirectionService _aimDirectionService = new AimDirectionService();
 
     private void Update() 
     {
+        Vector3 screenPosition = Input.mousePosition;
+        _aimDirectionService.UpdateDirections(screenPosition, transform.position, _currentWeapon.Range);
+        _currentWeapon.transform.rotation = Quaternion.LookRotation(_aimDirectionService.TargetDirection);
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Vector3 screenPosition = Input.mousePosition;
-            screenPosition.z = Camera.main.nearClipPlane + 1;
-            Ray cameraToMouseRay = Camera.main.ScreenPointToRay(screenPosition);
-
-            Vector3 cameraToPlayerVector = transform.position - Camera.main.transform.position;
-
-            _targetPosition = cameraToMouseRay.direction * _range + Camera.main.transform.position;
-            _shotDirection = Vector3.Normalize(cameraToMouseRay.direction * _range - cameraToPlayerVector);
-            _firedFromPosition = transform.position;
-            _maxDistance = Vector3.Magnitude(_targetPosition - transform.position);
-
-            _shotSparkParticle.Play();
-            _shotSparkParticle.transform.position = transform.position;
-            _firedFromPosition = transform.position;
-            _fired = true;
-        }
-
-        if (_fired) { MoveSpark(); }
-    }
-
-    private void MoveSpark()
-    {
-        _shotSparkParticle.transform.position += _shotDirection * _shotVelocity * Time.deltaTime;
-        float distanceFromTarget = Vector3.Magnitude(_shotSparkParticle.transform.position - _firedFromPosition);
-        if (distanceFromTarget >= _maxDistance) 
-        {
-            _shotSparkParticle.Stop();
-            _fired = false;
+            _currentWeapon.Shoot(_aimDirectionService.TargetDirection);
         }
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _bulletParticles;
+    [SerializeField] private float _range = 100f;
+    private float _bulletVelocity = 100f;
+    public float Range => _range;
+
+    private void Start() 
+    {
+        SetBulletSpeed();
+    }
+
+    public void Shoot(Vector3 direction)
+    {
+        _bulletParticles.transform.rotation = Quaternion.LookRotation(direction);
+        _bulletParticles.Play();
+    }
+    
+    private void SetBulletSpeed()
+    {
+        var particleMainSettings = _bulletParticles.main;
+        particleMainSettings.startSpeed = _bulletVelocity;
+    }
+}
